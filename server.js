62b18405b20bd5f94eaa72c38784b9346e150dc5
Kanjamad Bosel <npor25@yahoo.com>
2019-01-30 19:52:02 -0800

    change content-Type and then send a JavaScript object then transfer between the wires use JSON.stringify

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const http = require('http');
 
 const server = http.createServer((request, response) => {
-    response.setHeader('content-Type', 'text/html');
-    response.end('<h1>Helloooooo</h1>'); 
+    // console.log('headers', request.headers)
+    console.log('method', request.method)
+    console.log('url', request.url)
+    const user = {
+        name: 'John',
+        hobby: 'Skating'
+    }
+    response.setHeader('content-Type', 'application/json');
+    response.end(JSON.stringify(user)); 
 })
 
 server.listen(3000);
