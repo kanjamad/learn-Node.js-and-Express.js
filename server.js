595cdf73589c70d8b653b954749e6257a43531c4
Kanjamad Bosel <npor25@yahoo.com>
2019-01-30 21:24:00 -0800

    add semicolon

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) =>{
+app.get('/', (req, res) => {
     const user = {
         name: 'Sally',
         hobby:  'soccer'
     }
     res.send(user);
-})
+});
 
-app.listen(3000)
+app.listen(3000);
