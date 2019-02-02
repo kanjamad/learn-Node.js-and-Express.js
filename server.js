79303237cd51ba301cc02534402385319df903ef
Kanjamad Bosel <npor25@yahoo.com>
2019-02-02 15:03:51 -0800

    install body-parser

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 
 const app = express();
 
-
-app.use((req, res, next) => {
-    console.log('<h1>HELLOOOOOOOOO</h1>')
-    next();
-})
-
+app.use(bodyParser.urlencoded({extended: false}))
 app.get('/', (req, res) => {
-    res.send("testest");
+    res.send("getting root");
+});
+
+app.get('/profile', (req, res) => {
+    res.send("getting profile");
+});
+
+app.post('/profile', (req, res) => {
+    console.log(req.body)
+    const user = {
+        name: 'Sally',
+        hobby:  'soccer'
+    }
+    res.send(user);
 });
 
 app.listen(3000);
