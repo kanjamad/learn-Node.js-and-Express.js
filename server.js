1c697e9d482d8d055857a65418af85337fcc4bd7
Kanjamad Bosel <npor25@yahoo.com>
2019-02-06 22:07:51 -0800

    static assets

--- server.js
+++ server.js
@@ -2,14 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser')
 
 const app = express();
-
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json());
-
-app.get('/:id', (req, res) => {
-    console.log(req.params);
-    res.status(404).send("not found");
-});
+app.use(express.static(__dirname + '/public'))
 
 
 
